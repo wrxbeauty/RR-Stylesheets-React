a5221fd407dba6efe3449fb674688672baffb34a
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-03-27 21:04:58 -0400

    created one

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+const styledText = {
+  fontSize: "25px",
+  textAlign: "center",
+  margin: "40px",
+  border: "1px solid green"
+};
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 style={styledText}>Hello World</h1>
     </div>
   );
 }
